ecd2fabf2fa2c0bb7b92a74cef7166f11d48be8e
michp-ai <michael.j.penrose@gmail.com>
2021-09-17 08:10:08 +0100

    fix path issue on load for non-windows machines

--- utils_milestone2.py
+++ utils_milestone2.py
@@ -9,13 +9,15 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import precision_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+import os
 import copy
 
 
 def sector_mappings(df):
     # utility to clean the sector mappings in the data scraped from pdf's which were incorrect in about 1% of cases 
     # but can be corrected with these mappings
-    df_sectors = pd.read_csv("data\\sector_mappings.csv")
+    path = os.path.join('data', "sector_mappings.csv")
+    df_sectors = pd.read_csv(path)
     mdict = df_sectors.set_index('Ticker')['Sector'].to_dict()
     df['Sector'] = df['ticker']
     df['Sector'].replace(mdict, inplace=True)
@@ -23,14 +25,19 @@ def sector_mappings(df):
     return df
 
 def load_data(return_y_val=False, ohe_sector=True):
-    X_train = pd.read_pickle("data\\milestone_data_X_train.pkl")
+    path = os.path.join('data', "milestone_data_X_train.pkl")
+    X_train = pd.read_pickle(path)
     X_train = sector_mappings(X_train)    
-    y_train = pd.read_pickle("data\\milestone_data_y_train.pkl")
-    X_test = pd.read_pickle("data\\milestone_data_X_test.pkl")
+    path = os.path.join('data', "milestone_data_y_train.pkl")
+    y_train = pd.read_pickle(path)
+    path = os.path.join('data', "milestone_data_X_test.pkl")
+    X_test = pd.read_pickle(path)
     X_test = sector_mappings(X_test)
-    y_test = pd.read_pickle("data\\milestone_data_y_test.pkl")
+    path = os.path.join('data', "milestone_data_y_test.pkl")
+    y_test = pd.read_pickle(path)
     # validation data - we won't use this until the end
-    X_val = pd.read_pickle("data\\milestone_data_X_val.pkl")
+    path = os.path.join('data', "milestone_data_X_val.pkl")
+    X_val = pd.read_pickle(path)
     X_val = sector_mappings(X_val)
     
     if ohe_sector:
@@ -39,7 +46,8 @@ def load_data(return_y_val=False, ohe_sector=True):
         X_val = pd.concat([X_val,pd.get_dummies(X_val[['Sector']])],axis=1)
     
     if return_y_val:
-        y_val = pd.read_pickle("data\\milestone_data_y_val.pkl")
+        path = os.path.join('data', "milestone_data_y_val.pkl")
+        y_val = pd.read_pickle(path)
         return X_train, y_train, X_test, y_test, X_val, y_val
     else:
         return X_train, y_train, X_test, y_test, X_val
@@ -55,18 +63,6 @@ def threshold_precision(y, pred, percentile):
     threshold_precision = precision_score(y, pred_clf)
     
     return round(threshold_precision,4)
-
-def full_and_threshold_scoring(y, y_pred, percentile):
-    threshold = np.percentile(y_pred, percentile)
-    results = {}
-    y_clf = pred_to_clf_pred(y, threshold=1)
-    y_pred_clf = pred_to_clf_pred(y_pred, threshold=1)
-    results['default_precision'] = round(precision_score(y_clf, y_pred_clf),4)
-    results['default_return'] = round(np.mean(y[y_pred>1])-1,4)
-    results['threshold_precision'] = threshold_precision(y_clf, y_pred, percentile)
-    results['threshold_return'] = round(np.mean(y[y_pred>threshold])-1,4)
-    
-    return results
 
 def feature_engineering(df):
     df['Volume_over_Volume_MA50'] = df['Volume'] / df['Volume_MA50']
@@ -195,12 +191,8 @@ def run_model(current_selection, X_tr, X_te, y_train, y_test, model_type="regres
         # make the predictions
         y_pred_fundamental_train = model.predict(X_tr[current_selection])
         y_pred_fundamental_test = model.predict(X_te[current_selection])
-        # convert to classification
-        y_pred_clf_fundamental_train = pred_to_clf_pred(y_pred_fundamental_train, threshold=1)
-        y_pred_clf_fundamental_test = pred_to_clf_pred(y_pred_fundamental_test, threshold=1)
         train_scores = full_and_threshold_scoring(y_train,y_pred_fundamental_train,percentile)
         test_scores = full_and_threshold_scoring(y_test,y_pred_fundamental_test,percentile)
-        #print(c, test_scores['threshold_return'] )
     elif model_type=="classification":
         train_scores = {}
         test_scores = {}
